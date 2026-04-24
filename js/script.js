c759a0d2af0fbd7dc83e486d22a485f094e1a92f
aldhazahro <aldhazahro442@gmail.com>
2026-04-25 00:20:30 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+console.log("KRATÉE Jewelry Loaded ✨");
+document.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    if (this.getAttribute('href') === '#') {
+      e.preventDefault();
+    }
+  });
+});
